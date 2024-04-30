1621b1971ccec2c8d197f486402ad46da11d3c7c
Antonio Benincasa <benincasaantonio98@gmail.com>
2024-04-30 15:58:42 +0200

    feat: Improved level design and now the character lot at the right direction while moving

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 120.0
 const JUMP_VELOCITY = -300.0
 
+@onready var sprite = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -20,9 +22,19 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	_set_character_direction(direction)
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+
+func _set_character_direction(direction: float) -> void:
+	if direction < 0:
+		sprite.scale = Vector2(-1, 1)	
+	elif direction > 0: 
+		sprite.scale = Vector2(1,1) 
